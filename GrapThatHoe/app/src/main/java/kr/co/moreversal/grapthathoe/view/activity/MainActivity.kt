5dc5dbac7cca210459c0c2b533bd2e9491149931
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-13 23:47:54 +0900

    Add : File mvvm structure
    2021-10-13

--- GrapThatHoe/app/src/main/java/kr/co/moreversal/grapthathoe/view/activity/MainActivity.kt
+++ GrapThatHoe/app/src/main/java/kr/co/moreversal/grapthathoe/view/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package kr.co.moreversal.grapthathoe
+package kr.co.moreversal.grapthathoe.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kr.co.moreversal.grapthathoe.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
