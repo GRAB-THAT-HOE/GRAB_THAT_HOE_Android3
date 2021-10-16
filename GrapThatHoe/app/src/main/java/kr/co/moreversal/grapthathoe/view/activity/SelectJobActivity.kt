200147adf90da7cd2b95e4bc736aafb8fc0a236f
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-16 20:22:30 +0900

    Add : Select Job View
    2021-10-16

---
+++ GrapThatHoe/app/src/main/java/kr/co/moreversal/grapthathoe/view/activity/SelectJobActivity.kt
@@ -0,0 +1,12 @@
+package kr.co.moreversal.grapthathoe.view.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.co.moreversal.grapthathoe.R
+
+class SelectJobActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_job)
+    }
+}
