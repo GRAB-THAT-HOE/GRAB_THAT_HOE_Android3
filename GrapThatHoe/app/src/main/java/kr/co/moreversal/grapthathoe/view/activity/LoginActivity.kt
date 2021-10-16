c8966bf5ddd093f5ccdfc8033dfb7845ad689f5d
snack655 <80940200+snack655@users.noreply.github.com>
2021-10-16 16:29:07 +0900

    Add : FirstLogin View
    2021-10-16

---
+++ GrapThatHoe/app/src/main/java/kr/co/moreversal/grapthathoe/view/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package kr.co.moreversal.grapthathoe.view.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+}
