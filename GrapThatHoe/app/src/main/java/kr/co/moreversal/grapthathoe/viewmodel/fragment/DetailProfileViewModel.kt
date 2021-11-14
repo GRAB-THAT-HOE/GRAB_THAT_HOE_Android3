4db3cb27e95c24a105eb9df59af5ac70714fdd24
snack655 <80940200+snack655@users.noreply.github.com>
2021-11-14 18:20:44 +0900

    Add : DetailProfileFragment View design
    2021-11-14

--- GrapThatHoe/app/src/main/java/kr/co/moreversal/grapthathoe/viewmodel/fragment/DetailProfileViewModel.kt
+++ GrapThatHoe/app/src/main/java/kr/co/moreversal/grapthathoe/viewmodel/fragment/DetailProfileViewModel.kt
@@ -5,6 +5,11 @@ import kr.co.moreversal.grapthathoe.extension.SingleLiveEvent
 
 class DetailProfileViewModel: ViewModel() {
     val onBackEvent = SingleLiveEvent<Unit>()
+    val onCallEvent = SingleLiveEvent<Unit>()
+
+    fun onClickCall() {
+        onCallEvent.call()
+    }
 
     fun onClickBack() {
         onBackEvent.call()
